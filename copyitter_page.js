227e19c40bcbb6ccbc68ed09cb83a83b08adfbd8
Sonicfg <108159042+Sonicfg@users.noreply.github.com>
2022-11-23 20:45:06 -0600

    Add files via upload

---
+++ copyitter_page.js
@@ -0,0 +1,49 @@
+//TUS ENLACES DE FIREBASE
+var firebaseConfig = {
+      apiKey: "AIzaSyDcHdqnEnGTwvrmzOWCwlrH9yeksdXqQp4",
+      authDomain: "redsocial-b82d3.firebaseapp.com",
+      databaseURL: "https://redsocial-b82d3-default-rtdb.firebaseio.com",
+      projectId: "redsocial-b82d3",
+      storageBucket: "redsocial-b82d3.appspot.com",
+      messagingSenderId: "70247004637",
+      appId: "1:70247004637:web:3731619f8e231848189e34"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+user_name = localStorage.getItem("user_name");
+room_name = localStorage.getItem("room_name");
+function send() {
+      msg = document.getElementById("msg").value;
+      firebase.database().ref(room_name).push({
+            nombre: user_name,
+            mensaje: msg,
+            like: 0
+      });
+      document.getElementById("msg").value = "";
+}
+function getData() {
+      firebase.database().ref("/" + room_name).on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key; childData = childSnapshot.val(); if (childKey != "purpose") {
+                        firebase_message_id = childKey;
+                        message_data = childData;
+                        //Inicia código
+                        nombre = message_data["nombre"]
+                        mensaje = message_data["mensaje"]
+                        like = message_data["like"]
+                        row = "<h4>" + nombre + "</h4><h4 class='message_h4'>" + mensaje + "</h4> <button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'> 👍,🥳y🧐" + like + "</button>";
+                        document.getElementById("output").innerHTML += row;
+                        //Termina código .innerHTML
+                  }
+            });
+      });
+}
+getData();
+function updateLike(message_id) {
+      likes = document.getElementById(message_id).value;
+      updated_likes = Number(likes) + 1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like: updated_likes
+      });
+}
---
+++ copyitter_room.js
@@ -0,0 +1,51 @@
+
+//AGREGA TUS ENLACES DE FIREBASE AQUÍ
+var firebaseConfig = {
+      apiKey: "AIzaSyDcHdqnEnGTwvrmzOWCwlrH9yeksdXqQp4",
+      authDomain: "redsocial-b82d3.firebaseapp.com",
+      databaseURL: "https://redsocial-b82d3-default-rtdb.firebaseio.com",
+      projectId: "redsocial-b82d3",
+      storageBucket: "redsocial-b82d3.appspot.com",
+      messagingSenderId: "70247004637",
+      appId: "1:70247004637:web:3731619f8e231848189e34"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    user_name=localStorage.getItem("user_name");
+    document.getElementById("user_name").innerHTML="¡bienvenido!!!!!!😎👍👍👍pongan likes " +user_name;
+    function addRoom() {
+      room_name = document.getElementById("room_name").value;
+      
+    
+      firebase.database().ref("/").child(room_name).update({
+        purpose: "añadir el nombre de la sala"
+      });
+    
+      localStorage.setItem("room_name", room_name);
+    
+      window.location = "copyitter_page.html";
+    }
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = ""; snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Inicia el código
+                  row = "<div class='room_name' id='"+Room_names+"' onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div><hr>";
+                  document.getElementById("output").innerHTML +=row;
+                  //Finaliza el código
+            });
+      });
+}
+getData();
+function redirectToRoomName(room_name){
+      localStorage.setItem("room_name", room_name);
+      window.location="copyitter_page.html";
+}
+function desesionar(){
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+      window.location="index.html";
+}
